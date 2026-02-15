a85340b3de6e660728b65ca874a5536c2cb26530
tugudush <jerome2kph@gmail.com>
2026-02-16 05:47:39 +0800

    initial setup

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,36 +1,14 @@
-import { defineConfig, devices } from '@playwright/test';
+import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: './tests',
-  reporter: 'html',
+  testDir: "./tests",
   webServer: {
-    command: 'npm start',
-    url: 'http://localhost:5000/',
-    reuseExistingServer: true
+    command: "npm start",
+    url: "http://localhost:5000/",
+    reuseExistingServer: true,
   },
   use: {
-    baseURL: 'http://localhost:5000/',
+    baseURL: "http://localhost:5000/",
     headless: false
   },
-  // projects: [
-  //   {
-  //     name: 'chromium',
-  //     use: { ...devices['Desktop Chrome'], },
-  //     dependencies:[
-  //       'auth-setup'
-  //     ]
-  //   },
-  //   {
-  //     name: 'firefox',
-  //     use: { ...devices['Desktop Firefox'] },
-  //     dependencies:[
-  //       'auth-setup'
-  //     ]
-  //   },
-  //   {
-  //     name: 'auth-setup',
-  //     testMatch: 'tests/setup/Auth.setup.ts',
-  //   }
-  // ]
-
 });
